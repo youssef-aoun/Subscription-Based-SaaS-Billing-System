ff59f27dccdaf46bcd8f25939111c18a8f4eb1b5
Youssef Aoun <y.aoun@outlook.com>
2025-04-22 15:41:43 +0300

    Initializing the project

---
+++ src/main/java/com/youssef/Subscription_Based/SaaS/Billing/System/SubscriptionBasedSaaSBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.Subscription_Based.SaaS.Billing.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionBasedSaaSBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionBasedSaaSBillingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youssef/Subscription_Based/SaaS/Billing/System/SubscriptionBasedSaaSBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.Subscription_Based.SaaS.Billing.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionBasedSaaSBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
